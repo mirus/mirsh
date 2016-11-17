f5254466da3eb196b288e4155c8ba617e02556ff
Joshua Beitler <joshbeitler@gmail.com>
2016-11-17 12:06:01 -0700

    Start parsing argumnets

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,35 @@ extern crate liner;
 use liner::Context;
 use std::process::{Command, exit};
 use std::string::String;
+use std::env;
+use std::path::Path;
 
 fn exec(proc_name: String) {
+    let mut args_iter = proc_name.split_whitespace();
+    let to_exec = args_iter.next();
     // println!("{}", proc_name);
-    let status = Command::new(proc_name)
+    let status = Command::new(to_exec.unwrap())
         .status()
         .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
 
     // println!("process exited with: {}", status);
 }
 
+fn build_prompt() -> String {
+    String::from("[prompt]$ ")
+}
+
+// built in functions
+fn cd(new_dir: String) {
+    let root = Path::new(&new_dir);
+    assert!(env::set_current_dir(&root).is_ok());
+}
+
 fn main() {
     let mut con = Context::new();
 
     loop {
-        let res = con.read_line("[prompt]$ ", &mut |_| {}).unwrap();
+        let res = con.read_line(build_prompt(), &mut |_| {}).unwrap();
 
         if res.is_empty() {
             break;
@@ -25,6 +39,7 @@ fn main() {
 
         match res.as_ref() {
             "exit" => exit(0),
+            "cd" => cd(res.clone()),
             _ => exec(res.clone()),
         }
 
